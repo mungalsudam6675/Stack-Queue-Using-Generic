ea2d1dcba9cbf212b78b48d877479c078deefee9
Mungal Sudam <MUngalsudam6675@gmail.com>
2022-02-11 16:54:57 +0530

    MyStack class

---
+++ MyStack.java
@@ -0,0 +1,24 @@
+package JavaPractice;
+
+public class MyStack {
+	
+	private final MyLinkedList myLinkedList;
+	
+	public MyStack() {
+		this.myLinkedList = new MyLinkedList();
+	}
+
+	public void push(INode myNode) {
+		myLinkedList.add(myNode);
+		
+	}
+	
+	public void printStack() {
+		myLinkedList.printMyNode();
+	}
+
+	public INode peak() {
+		return myLinkedList.head;
+		
+	}
+}
